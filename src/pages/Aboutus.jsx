f7d7c42c92480a87a2e255ac7defcac00f12f1e9
Devashish Upreti <devashup68@gmail.com>
2025-06-02 13:36:09 +0545

    worked on about us page

--- src/pages/Aboutus.jsx
+++ src/pages/Aboutus.jsx
@@ -1,10 +1,14 @@
-import React from "react";
-import { DiBackbone } from "react-icons/di";
+import React, { useState } from "react";
+import Skeleton from "react-loading-skeleton";
+import "react-loading-skeleton/dist/skeleton.css";
 
 const Aboutus = () => {
   // throw new Error("about us error!");
+  const [isLoading, setIsLoading] = useState(false);
+  const [imageLoaded, setImageLoaded] = useState(false);
+
   return (
-    <div className="container my-4 flex flex-col gap-4 lg:flex-row lg:gap-2">
+    <div className="container my-4 flex flex-col gap-4 py-10 lg:flex-row lg:gap-2">
       <div className="flex-1">
         <div className="flex flex-1 flex-col">
           <div className="font-semi-bold mb-6 text-[24px] lg:mb-10 lg:text-[40px]">
@@ -22,11 +26,22 @@ const Aboutus = () => {
           </p>
         </div>
       </div>
+      {/* {
       <img
-        src="/assets/aboutuscabin-2.jpg"
+        src="/assets/aboutuscabin-2.jp"
         alt=""
         className="w-full lg:w-1/3"
-      />
+      /> ||  <Skeleton height={100} width={100} />
+      } */}
+      <div className="w-full lg:w-1/3">
+        {!imageLoaded && <Skeleton height={480} />}
+        <img
+          src="/assets/aboutuscabin-2.jpg"
+          alt="about us"
+          className={`w-full  duration-300 ${imageLoaded ? "opacity-100" : "absolute opacity-0"}`}
+          onLoad={() => setImageLoaded(true)}
+        />
+      </div>
     </div>
   );
 };
